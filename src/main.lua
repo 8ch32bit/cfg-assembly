46bd38b7e2533f82f5f05d8c2a611a3180ab3934
cfgtsp <69192198+cfgtsp5192@users.noreply.github.com>
2023-03-05 16:33:42 +0800

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -1,9 +1,4 @@
-local lmc =        require('lmc');
-local readFile =   require('readFile');
-local lmc_parser = require('lmcparser');
+local LMC = require('init');
+local exec = LMC.wrapFile("example.lmc");
 
-local file = readFile("example.lmc");
-local process = lmc.new("Monkey", lmc_parser.parse(file));
-local execute = process:wrap(process:getProto("main"));
-
-execute();
+exec();
