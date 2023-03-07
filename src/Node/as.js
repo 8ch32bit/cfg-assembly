88ccb086170f347370e9c8205aac2328f6b4cdd6
cfgtsp <69192198+cfgtsp64@users.noreply.github.com>
2023-03-07 13:56:40 +0800

    Update as.js

--- src/Node/as.js
+++ src/Node/as.js
@@ -1,3 +1,42 @@
+const exec = require('child_process').exec;
+
+async function _sleep(time) {
+	await new Promise(() => resolve(0), time);
+}
+
+function sleep(time) {
+	exec(`sleep ${time}`);
+}
+
+function _concat(t, seperator, a, b) {
+	a = a || 1; b = b || t.length;
+	seperator = seperator || "";
+	var s = "";
+	for (var idx = a; idx <= b; idx++) {
+		s = s + t[idx] + seperator;
+	}
+	return s;
+}
+
+function concat(t, encode, a, b) { // Experimental concanation functions for better use
+	if (encode === true) return _concat(t, "", a, b);
+	a = a || 1; b = b || t.length;
+	seperator = seperator || "";
+	var s = "";
+	for (var idx = a; idx <= b; idx++) {
+		s = s + (encode && t[idx].charCodeAt())|| "";
+	}
+	return s;
+}
+
+function splitArr(t, a, b) {
+	var newArr = [];
+	for (var idx = a; idx <= b; idx++) {
+		newArr[idx] = t[idx];
+	}
+	return newArr;
+}
+
 class instruction {
 	constructor(op, a, b, c) {
 		this.op = op;
@@ -7,16 +46,13 @@ class instruction {
 	}
 	
 	deconstruct() {
-		const op = this.op;
-		const a = this.a || null;
-		const b = this.b || null;
-		const c = this.c || null;
+		const op, a, b, c = this.op, this.a || null, this.b || null, this.c || null;
 		return op, a, b, c;
 	}
 	
 	stringify() {
 		const op, a, b, c = this.deconstruct();
-		return `instruction: [op: ${op}, a: ${a}, b: ${b}, c: ${b}]`;
+		return `instruction: [opcode: ${op}, a: ${a}, b: ${b}, c: ${b}]`;
 	}
 }
 
@@ -33,6 +69,12 @@ class as {
 		}
 	}
 	
+	getProto(name) {
+		for (var idx = 1; idx <= this.protos.length; idx++) {
+			if (this.protos[idx].name === name) return self.protos[idx];
+		}
+	}
+	
 	wrap(proto) {
 		proto = proto || this.getProto(proto) || this.getProto();
 		const instructions = proto.instructions;
@@ -43,10 +85,65 @@ class as {
 			const setpc = (x) => pc = x;
 			while(true) {
 				addpc();
-				
+				if (pc > max) {
+					break;
+				} else {
+					const opcode, a, b, c = instructions[pc].deconstruct();
+					switch(opcode) {
+						case 0:
+							this.memory[a] = this.memory[b] + this.memory[c]; break;
+						case 1:
+							this.memory[a] = this.memory[b] + this.memory[c]; break;
+						case 2:
+							this.memory[a] = this.memory[b] - this.memory[c]; break;
+						case 3:
+							this.memory[a] = Math.floor(this.memory[b] / this.memory[c]); break;
+						case 4:
+							this.memory[a] = this.memory[b || a]; break;
+						case 5:
+							this.memory[a] = 0; break;
+						case 6:
+							this.memory[a] = this.memory[b]; break;
+						case 7:
+							this.memory[a] = b; break;
+						case 8: // COUT
+							process.stdout.write(JSON.stringify(this.memory[a])); break;
+						case 9: // COUTNL
+							process.stdout.write(JSON.stringify(this.memory[a]) + "\n"); break;
+						case 10: // COUTNLRANGE
+							process.stdout.write(concat(this.memory, false, a, b) + "\n"); break;
+						case 11: // COUTNLRANGESTR
+							process.stdout.write(concat(this.memory, true, a, b) + "\n"); break;
+						case 12: // JMP
+							addpc(a); break;
+						case 13: // SETPC
+							setpc(a); break;
+						case 14: // RESETPC
+							setpc(0); break;
+						case 15: // HALT
+							sleep(a); break;
+						case 16:
+							setpc(max); break;
+						case 17:
+							if (!(this.memory[a] < this.memory[b])) {addpc(c);} break;
+						case 18:
+							if (!(this.memory[a] > this.memory[b])) {addpc(c);} break;
+						case 19:
+							if (!(this.memory[a] <= this.memory[b])) {addpc(c);} break;
+						case 20:
+							if (!(this.memory[a] >= this.memory[b])) {addpc(c);} break;
+						case 21:
+							if (!(this.memory[a] === this.memory[b])) {addpc(c);} break;
+						case 22:
+							this.getProto(a).exec(); break;
+						case 23:
+							setpc(max);
+							return splitArr(this.memory, a || 0, b || this.memory.length);
+					}
+				}
 			}
 		}
 	}
-};
+}
 
-module.exports = as;
+module.exports = {as: as, instruction: instruction};
