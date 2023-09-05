a30a576fab723fe4060db42726b2801e57de8627
cfgtsp <69192198+cfgtsp64@users.noreply.github.com>
2023-09-05 23:34:23 +0800

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,35 @@
+local as       = require('src.as');
+local parser   = require('src.parser');
+
+local readFile = require('src.readFile');
+
+local function newAssembler(src, name)
+	return as.new(name, parser.parse(src));
+end
+
+local function newAssemblerFile(path, name)
+	return newAssembler(readFile(path), name);
+end
+
+local function wrap(assembler)
+	local main  = assembler:getProto("main");
+	local setup = assembler:getProto("setup");
+	
+	return function()
+		if setup then
+			setup.exec();
+		end
+		
+		return main.exec();
+	end
+end
+
+local function wrapSrc(src, name)
+	return wrap(newAssembler(src, name))
+end
+
+local function wrapFile(src, name)
+	return wrap(newAssemblerFile(src, name));
+end
+
+return {wrap = wrap, wrapSrc = wrapSrc, wrapFile = wrapFile};
